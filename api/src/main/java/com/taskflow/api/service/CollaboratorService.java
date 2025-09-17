3d1343894a6a8d3f7296f48c1b25bb3fe7ef7fb1
gibranmenezes <gibranquimica@gmail.com>
2025-09-17 14:20:23 +0100

    feat: implementing update and delete methods

--- api/src/main/java/com/taskflow/api/service/CollaboratorService.java
+++ api/src/main/java/com/taskflow/api/service/CollaboratorService.java
@@ -1,10 +1,8 @@
 package com.taskflow.api.service;
 
-import com.taskflow.api.domain.collaborator.Collaborator;
-import com.taskflow.api.domain.collaborator.CollaboratorCreationRequest;
-import com.taskflow.api.domain.collaborator.CollaboratorCreationResponse;
-import com.taskflow.api.domain.collaborator.CollaboratorDetails;
+import com.taskflow.api.domain.collaborator.*;
 import com.taskflow.api.domain.user.User;
+import com.taskflow.api.mapper.CollaboratorMapper;
 import com.taskflow.api.respository.CollaboratorRepository;
 import com.taskflow.api.respository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
@@ -22,9 +20,10 @@ public class CollaboratorService {
 
     private final UserRepository userRepository;
     private final CollaboratorRepository collaboratorRepository;
+    private final CollaboratorMapper mapper;
 
 
-    public CollaboratorCreationResponse registerEmployee(CollaboratorCreationRequest request) {
+    public CollaboratorCreationResponse createCollaborator(CollaboratorCreationRequest request) {
         var userToPersist = User.builder()
                 .username(request.username())
                 .email(request.email())
@@ -33,7 +32,8 @@ public class CollaboratorService {
 
         var userPersisted = userRepository.save(userToPersist);
 
-        var manager = this.findById(request.managerId());
+        var manager = collaboratorRepository.findById(request.managerId())
+                .orElseThrow(() -> new EntityNotFoundException("Manager not found"));
 
         var employee = Collaborator.builder()
                 .name(request.name())
@@ -46,16 +46,36 @@ public class CollaboratorService {
 
     }
 
-    public Collaborator findById(UUID id) {
-        return collaboratorRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Employee not found"));
+    public CollaboratorDetails getCollaboratorById(UUID id) {
+        var collaborator =  collaboratorRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Collaborator not found"));
+
+        return new CollaboratorDetails(collaborator);
     }
 
-    public Page<CollaboratorDetails> getEmployees(int page, int size) {
+    public Page<CollaboratorDetails> getAllCollaborators(int page, int size) {
         var pageable = PageRequest.of(page, size);
 
         Page<Collaborator> employees = collaboratorRepository.findAll(pageable);
 
         return employees.map(CollaboratorDetails::new);
 
+    }
+
+    public void updateCollaborator(UUID id, CollaboratorUpdateRequest request) {
+        var collaborator =  collaboratorRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Collaborator not found"));
+
+      mapper.updateCollaboratorFromDto(request, collaborator);
+
+      collaboratorRepository.save(collaborator);
+
+    }
+
+    public void deleteCollaborator(UUID id) {
+        var collaborator =  collaboratorRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Collaborator not found"));
+
+        collaborator.inactivate();
     }
 }
