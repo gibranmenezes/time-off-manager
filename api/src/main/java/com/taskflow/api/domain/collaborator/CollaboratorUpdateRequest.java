01e3db5cd521a65536a0114f206346837314e3e3
gibranmenezes <gibranquimica@gmail.com>
2025-09-17 14:21:24 +0100

    feat: created update dto

---
+++ api/src/main/java/com/taskflow/api/domain/collaborator/CollaboratorUpdateRequest.java
@@ -0,0 +1,7 @@
+package com.taskflow.api.domain.collaborator;
+
+import com.taskflow.api.domain.enums.Role;
+
+public record CollaboratorUpdateRequest(String username, String userEmail, String userPassword, Role userRole,
+                                        String name, String department, Collaborator manager) {
+}
