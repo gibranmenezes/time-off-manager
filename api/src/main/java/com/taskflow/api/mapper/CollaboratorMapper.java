b1d3257bc50e4caa3170f8af02c624e4ebf27a5e
gibranmenezes <gibranquimica@gmail.com>
2025-09-18 17:04:49 +0100

    feat: implementing new mappers

--- api/src/main/java/com/taskflow/api/mapper/CollaboratorMapper.java
+++ api/src/main/java/com/taskflow/api/mapper/CollaboratorMapper.java
@@ -1,13 +1,15 @@
 package com.taskflow.api.mapper;
 
 import com.taskflow.api.domain.collaborator.Collaborator;
+import com.taskflow.api.domain.collaborator.CollaboratorCreationResponse;
+import com.taskflow.api.domain.collaborator.CollaboratorDetails;
 import com.taskflow.api.domain.collaborator.CollaboratorUpdateRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface CollaboratorMapper {
 
     @Mapping(target = "user.email", source = "userEmail")
@@ -16,5 +18,11 @@ public interface CollaboratorMapper {
     @Mapping(target = "name", source = "name")
     @Mapping(target = "department", source = "department")
     @Mapping(target = "manager", source = "manager")
-    void updateCollaboratorFromDto(CollaboratorUpdateRequest request, @MappingTarget Collaborator collaborator);
+    void updatedCollaboratorFromDto(CollaboratorUpdateRequest request, @MappingTarget Collaborator collaborator);
+
+    @Mapping(target = "username", source = "user.username")
+    CollaboratorCreationResponse toCreationResponse(Collaborator collaborator);
+
+    @Mapping(target = "managerName", expression = "java(collaborator.getManager() != null ? collaborator.getManager().getName() : null)")
+    CollaboratorDetails toDetails(Collaborator collaborator);
 }
