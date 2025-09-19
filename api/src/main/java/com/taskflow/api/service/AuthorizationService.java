1f59365629b2dc16160c4d5eb4ddde7cf449ee54
gibranmenezes <gibranquimica@gmail.com>
2025-09-19 14:36:18 +0100

    feat: implementing auth service

--- api/src/main/java/com/taskflow/api/service/AuthorizationService.java
+++ api/src/main/java/com/taskflow/api/service/AuthorizationService.java
@@ -1,30 +1,18 @@
-package com.taskflow.api.service.auth;
+package com.taskflow.api.service;
 
 import com.taskflow.api.domain.exception.ValidationException;
 import com.taskflow.api.domain.user.User;
+import com.taskflow.api.service.auth.AuthorizationPolicy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-/**
- * Service that centralizes authorization logic using policy pattern.
- * This service maintains a list of authorization policies and checks if any of them grant access.
- */
+
 @Service
 @RequiredArgsConstructor
 public class AuthorizationService {
 
-    /**
-     * Checks if the user has access to the resource using a list of policies.
-     * Access is granted if any policy returns true.
-     *
-     * @param user User attempting to access the resource
-     * @param resource The resource being accessed
-     * @param policies List of policies to check
-     * @param errorMessage Error message to throw if access is denied
-     * @throws ValidationException if access is denied
-     */
     public <T> void checkAccess(User user, T resource, List<AuthorizationPolicy<T>> policies, String errorMessage) {
         boolean hasAccess = policies.stream()
                 .anyMatch(policy -> policy.hasAccess(user, resource));
