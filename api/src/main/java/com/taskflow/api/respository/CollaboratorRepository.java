925af93b592bb2764058bc8d4387cec32b775ca1
gibranmenezes <gibranquimica@gmail.com>
2025-09-19 10:51:11 +0100

    feat: adding query method

--- api/src/main/java/com/taskflow/api/respository/CollaboratorRepository.java
+++ api/src/main/java/com/taskflow/api/respository/CollaboratorRepository.java
@@ -2,10 +2,13 @@ package com.taskflow.api.respository;
 
 import com.taskflow.api.domain.collaborator.Collaborator;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CollaboratorRepository extends JpaRepository<Collaborator, Long> {
 
-
+    @Query("select c.id from Collaborator c where c.user.id = :userId")
+    Optional<Long> findCollaboratorIdByUserId(Long userId);
 }
