7e3246eb47a2bc7f577d13bea543a5513a75d818
gibranmenezes <gibranquimica@gmail.com>
2025-09-19 12:22:00 +0100

    feat: adding userdetails

--- api/src/main/java/com/taskflow/api/domain/user/User.java
+++ api/src/main/java/com/taskflow/api/domain/user/User.java
@@ -6,8 +6,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.Instant;
+import java.util.Collection;
+import java.util.List;
 
 
 @Entity
@@ -16,7 +21,7 @@ import java.time.Instant;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -58,5 +63,39 @@ public class User {
 
     public void inactivate() {
         this.active = false;
+    }
+
+    public boolean isActive() {
+        return this.active;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == Role.ADMIN)
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else if (this.role == Role.MANAGER)
+            return List.of(new SimpleGrantedAuthority("ROLE_MANAGER"), new SimpleGrantedAuthority("ROLE_USER"));
+        else
+            return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.active;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.active;
     }
 }
--- api/src/main/java/com/taskflow/api/service/user/UserService.java
+++ api/src/main/java/com/taskflow/api/service/user/UserService.java
@@ -1,22 +1,46 @@
 package com.taskflow.api.service.user;
 
 import com.taskflow.api.domain.enums.Role;
+import com.taskflow.api.domain.exception.ValidationException;
 import com.taskflow.api.domain.user.User;
 import com.taskflow.api.respository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities(user.getRole().name())
+                .accountExpired(!user.isActive())
+                .accountLocked(!user.isActive())
+                .credentialsExpired(!user.isActive())
+                .disabled(!user.isActive())
+                .build();
+
+    }
 
     public User createUser(String username, String email, String password, Role role) {
 
-        if (userRepository.existsByEmail(email)) {
-            throw new IllegalArgumentException("Email already exists");
+        if (userRepository.findByUsername(username).isPresent()) {
+            throw new ValidationException("Username already taken", ValidationException.ValidationErrorType.INVALID_INPUT);
         }
 
         var user = User.builder()
@@ -46,5 +70,7 @@ public class UserService {
 
         return userRepository.save(user);
     }
+
+    
 
 }
