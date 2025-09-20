68ddf2409c8342f32e77685b209d0f7a53c6d96c
gibranmenezes <gibranquimica@gmail.com>
2025-09-20 15:37:20 +0100

    feta: adding new query

--- api/src/main/java/com/taskflow/api/respository/CollaboratorRepository.java
+++ api/src/main/java/com/taskflow/api/respository/CollaboratorRepository.java
@@ -1,14 +1,44 @@
 package com.taskflow.api.respository;
 
 import com.taskflow.api.domain.collaborator.Collaborator;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.Instant;
 import java.util.Optional;
-import java.util.UUID;
 
 public interface CollaboratorRepository extends JpaRepository<Collaborator, Long> {
 
     @Query("select c.id from Collaborator c where c.user.id = :userId")
     Optional<Long> findCollaboratorIdByUserId(Long userId);
+
+    @Query("""
+    select c
+    from Collaborator c
+    join c.user u
+    where (:name is null or lower(c.name) like lower(concat('%', :name, '%')))
+      and (:username is null or lower(u.username) like lower(concat('%', :username, '%')))
+      and (:email is null or lower(u.email) like lower(concat('%', :email, '%')))
+      and (:department is null or lower(c.department) like lower(concat('%', :department, '%')))
+      and (:managerId is null or c.manager.id = :managerId)
+      and (:createdFrom is null or c.createdAt >= :createdFrom)
+      and (:createdTo is null or c.createdAt <= :createdTo)
+      and (:userRole is null or u.role = upper(:userRole) )
+      and (:active is null or c.active = :active)
+    """)
+    Page<Collaborator> findAllWithFilters(
+            @Param("name") String name,
+            @Param("username") String username,
+            @Param("email") String email,
+            @Param("department") String department,
+            @Param("managerId") Long managerId,
+            @Param("createdFrom") Instant createdFrom,
+            @Param("createdTo") Instant createdTo,
+            @Param("userRole") String userRole,
+            @Param("active") Boolean active,
+            Pageable pageable
+    );
 }
