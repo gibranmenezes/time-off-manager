eee0bdd1b205f8246ebf04589635015151b35fa6
gibranmenezes <gibranquimica@gmail.com>
2025-09-20 15:36:28 +0100

    feat: implementing documentation

--- api/src/main/java/com/taskflow/api/web/controller/AuthController.java
+++ api/src/main/java/com/taskflow/api/web/controller/AuthController.java
@@ -1,11 +1,48 @@
 package com.taskflow.api.web.controller;
 
+import com.taskflow.api.service.AuthorizationService;
+import com.taskflow.api.web.dtos.AppResponse;
+import com.taskflow.api.web.dtos.LoginRequest;
+import com.taskflow.api.web.dtos.TokenResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Tag(name = "Authentication", description = "Endpoints for user authentication")
 public class AuthController {
+
+    private final AuthorizationService authorizationService;
+
+        @Operation(
+            summary = "User login",
+            description = "Authenticates a user and returns a JWT token if credentials are valid."
+        )
+        @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Login successful",
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = AppResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid login data",
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = AppResponse.class))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized - invalid credentials",
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = AppResponse.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = AppResponse.class)))
+        })
+    @PostMapping("/login")
+    public ResponseEntity<AppResponse<TokenResponse>> login(@RequestBody @Valid LoginRequest request) {
+        var response = authorizationService.login(request);
+        return AppResponse.ok("Login successfull", response).getResponseEntity();
+    }
 }
--- api/src/main/java/com/taskflow/api/web/controller/CollaboratorController.java
+++ api/src/main/java/com/taskflow/api/web/controller/CollaboratorController.java
@@ -1,11 +1,14 @@
 package com.taskflow.api.web.controller;
 
-import com.taskflow.api.domain.collaborator.CollaboratorCreationRequest;
-import com.taskflow.api.domain.collaborator.CollaboratorCreationResponse;
-import com.taskflow.api.domain.collaborator.CollaboratorDetails;
-import com.taskflow.api.domain.collaborator.CollaboratorUpdateRequest;
+import com.taskflow.api.domain.collaborator.*;
 import com.taskflow.api.service.CollaboratorService;
 import com.taskflow.api.web.dtos.AppResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +19,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/collaborators")
 @RequiredArgsConstructor
+@Tag(name = "Collaborators", description = "Endpoints for managing collaborators")
 public class CollaboratorController {
 
     private final CollaboratorService collaboratorService;
 
+    @Operation(summary = "Create a new device", description = "Adds a new device to the system.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Device created successfully",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AppResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid device data provided",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AppResponse.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AppResponse.class)))
+    })
     @PostMapping
     public ResponseEntity<AppResponse<CollaboratorCreationResponse>> register(@RequestBody @Valid CollaboratorCreationRequest request) {
         var response = collaboratorService.createCollaborator(request);
@@ -27,36 +43,78 @@ public class CollaboratorController {
 
     }
 
+    @Operation(
+        summary = "List collaborators",
+        description = "Returns a paginated list of collaborators. Supports filtering by name, username, email, department, manager, creation date, and active status."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Collaborators found",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AppResponse.class))),
+        @ApiResponse(responseCode = "500", description = "Internal server error",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AppResponse.class)))
+    })
     @GetMapping
-    public ResponseEntity<AppResponse<List<CollaboratorDetails>>> getCollaborators(@RequestParam(defaultValue = "0") int page,
-                                                                               @RequestParam(defaultValue = "10") int size) {
-        var collaborators = collaboratorService.getAllCollaborators(page, size);
-
+    public ResponseEntity<AppResponse<List<CollaboratorDetails>>> getCollaborators(@ModelAttribute CollaboratorFilter filter, @RequestParam(defaultValue = "0") int page,
+                                                                                   @RequestParam(defaultValue = "10") int size) {
+        var collaborators = collaboratorService.getAllCollaborators(filter,page, size);
         return AppResponse.ok("Employees found", collaborators.getContent())
                 .buildParametersPagination(collaborators.getNumber(), collaborators.getSize(), collaborators.getTotalElements(), collaborators.getTotalPages())
                 .getResponseEntity();
     }
 
+    @Operation(
+        summary = "Get collaborator by ID",
+        description = "Returns details of a collaborator by their ID."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Collaborator found",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AppResponse.class))),
+        @ApiResponse(responseCode = "404", description = "Collaborator not found",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AppResponse.class))),
+        @ApiResponse(responseCode = "500", description = "Internal server error",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AppResponse.class)))
+    })
     @GetMapping("/{id}")
     public ResponseEntity<AppResponse<CollaboratorDetails>> getCollaboratorById(@PathVariable Long id) {
         var collaborator = collaboratorService.getCollaboratorById(id);
-
         return AppResponse.ok("Collaborator found!", collaborator).getResponseEntity();
-
     }
 
+    @Operation(
+        summary = "Update collaborator",
+        description = "Updates the details of a collaborator by their ID."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Collaborator updated successfully",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AppResponse.class))),
+        @ApiResponse(responseCode = "400", description = "Invalid update data",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AppResponse.class))),
+        @ApiResponse(responseCode = "404", description = "Collaborator not found",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AppResponse.class))),
+        @ApiResponse(responseCode = "500", description = "Internal server error",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AppResponse.class)))
+    })
     @PatchMapping("/{id}")
-    public ResponseEntity<AppResponse<Void>> updateEmployee(@PathVariable Long id, @RequestBody CollaboratorUpdateRequest request) {
+    public ResponseEntity<AppResponse<Void>> updateEmployee(@PathVariable Long id, CollaboratorUpdateRequest request) {
         collaboratorService.updateCollaborator(id, request);
-
         return AppResponse.<Void>ok("Collaborator updated successfully!", null).getResponseEntity();
-
     }
 
+    @Operation(
+        summary = "Delete collaborator",
+        description = "Deletes a collaborator by their ID."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Collaborator deleted successfully",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AppResponse.class))),
+        @ApiResponse(responseCode = "404", description = "Collaborator not found",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AppResponse.class))),
+        @ApiResponse(responseCode = "500", description = "Internal server error",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AppResponse.class)))
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<AppResponse<Void>>  deleteCollaborator(@PathVariable Long id) {
         collaboratorService.deleteCollaborator(id);
         return AppResponse.<Void>ok("Collaborator excluded successfully!", null).getResponseEntity();
-
     }
 }
