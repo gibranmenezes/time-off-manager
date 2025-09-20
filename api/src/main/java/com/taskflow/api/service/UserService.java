76588c8d2955e2e4312c04cf5be7d2add3af9e23
gibranmenezes <gibranquimica@gmail.com>
2025-09-20 15:38:32 +0100

    refact: modifying user return

--- api/src/main/java/com/taskflow/api/service/UserService.java
+++ api/src/main/java/com/taskflow/api/service/UserService.java
@@ -1,6 +1,7 @@
 package com.taskflow.api.service;
 
 import com.taskflow.api.domain.enums.Role;
+import com.taskflow.api.domain.exception.ValidationErrorType;
 import com.taskflow.api.domain.exception.ValidationException;
 import com.taskflow.api.domain.user.User;
 import com.taskflow.api.respository.UserRepository;
@@ -22,31 +23,21 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        var user = userRepository.findByUsername(username)
+        return userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
-        return org.springframework.security.core.userdetails.User
-                .withUsername(user.getUsername())
-                .password(user.getPassword())
-                .authorities(user.getRole().name())
-                .accountExpired(!user.isActive())
-                .accountLocked(!user.isActive())
-                .credentialsExpired(!user.isActive())
-                .disabled(!user.isActive())
-                .build();
 
     }
 
     public User createUser(String username, String email, String password, Role role) {
 
         if (userRepository.findByUsername(username).isPresent()) {
-            throw new ValidationException("Username already taken", ValidationException.ValidationErrorType.INVALID_INPUT);
+            throw new ValidationException("Username already taken", ValidationErrorType.INVALID_INPUT);
         }
 
         var user = User.builder()
                 .username(username)
                 .email(email)
-                .password(password)
+                .password(passwordEncoder.encode(password))
                 .role(role)
                 .build();
 
