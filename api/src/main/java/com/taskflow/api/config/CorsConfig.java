4fcdce1f24eea6cb633dec691848e29370cf3dab
gibranmenezes <gibranquimica@gmail.com>
2025-09-21 15:34:50 +0100

    feat: adding path into cors policy

--- api/src/main/java/com/taskflow/api/config/CorsConfig.java
+++ api/src/main/java/com/taskflow/api/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "http://localhost:3001", "http://frontend:3001")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
