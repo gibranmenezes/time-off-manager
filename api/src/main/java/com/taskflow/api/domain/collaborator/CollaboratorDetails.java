3ed02bb6bda90146ff6624a03823e1da506714eb
gibranmenezes <gibranquimica@gmail.com>
2025-09-21 12:06:53 +0100

    refact: including new fields in fitler and details

--- api/src/main/java/com/taskflow/api/domain/collaborator/CollaboratorDetails.java
+++ api/src/main/java/com/taskflow/api/domain/collaborator/CollaboratorDetails.java
@@ -4,5 +4,5 @@ import com.taskflow.api.domain.enums.Role;
 
 import java.time.Instant;
 
-public record CollaboratorDetails(Long id, String name, String email, String department, Role role , String managerName, Instant createdAt) {
+public record CollaboratorDetails(Long id, String name,String username, String email, String department, Role role , boolean active, String managerName, Instant createdAt) {
 }
--- api/src/main/java/com/taskflow/api/domain/collaborator/CollaboratorFilter.java
+++ api/src/main/java/com/taskflow/api/domain/collaborator/CollaboratorFilter.java
@@ -5,7 +5,8 @@ import com.taskflow.api.domain.enums.Role;
 import java.time.LocalDate;
 import java.util.Objects;
 
-public record CollaboratorFilter(String name, String username, String email, String department, Long managerId, Role userRole,
+public record CollaboratorFilter(Long collaboratorId, String name, String username, String email, String department, Long managerId,
+                                 String managerName, Role userRole,
                                  LocalDate createdAtStart, LocalDate createdAtEnd, Boolean active) {
     public CollaboratorFilter {
         active = Objects.requireNonNullElse(active, true);
