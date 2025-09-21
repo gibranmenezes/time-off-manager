8d53d69687955d303cf41ca57b2bf24bb1da7919
gibranmenezes <gibranquimica@gmail.com>
2025-09-21 12:07:33 +0100

    refact: modifying mapping

--- api/src/main/java/com/taskflow/api/mapper/CollaboratorMapper.java
+++ api/src/main/java/com/taskflow/api/mapper/CollaboratorMapper.java
@@ -12,20 +12,9 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface CollaboratorMapper {
 
-    @Mapping(target = "user.email", source = "userEmail")
-    @Mapping(target = "user.password", source = "userPassword")
-    @Mapping(target = "user.role", source = "userRole")
-    @Mapping(target = "name", source = "name")
-    @Mapping(target = "department", source = "department")
-    @Mapping(target = "manager.id", source = "managerId")
-    void updatedCollaboratorFromDto(CollaboratorUpdateRequest request, @MappingTarget Collaborator collaborator);
-
-    @Mapping(target = "username", source = "user.username")
-    @Mapping(target = "managerId", source = "manager.id")
-    CollaboratorCreationResponse toCreationResponse(Collaborator collaborator);
-
     @Mapping(target = "managerName", expression = "java(collaborator.getManager() != null ? collaborator.getManager().getName() : null)")
     @Mapping(target = "role", source = "user.role")
     @Mapping(target = "email", source = "user.email")
+    @Mapping(target = "username", source = "user.username")
     CollaboratorDetails toDetails(Collaborator collaborator);
 }
--- api/src/main/java/com/taskflow/api/mapper/VacationMapper.java
+++ api/src/main/java/com/taskflow/api/mapper/VacationMapper.java
@@ -1,12 +1,17 @@
 package com.taskflow.api.mapper;
 
 
+import com.taskflow.api.domain.vacation.Vacation;
 import com.taskflow.api.domain.vacation.VacationResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface VacationMapper {
 
-    VacationResponse toResponse(com.taskflow.api.domain.vacation.Vacation vacation);
+    @Mapping(target = "collaboratorName", source = "collaborator.name")
+    @Mapping(target = "collaboratorId", source = "collaborator.id")
+    @Mapping(target = "status", source = "vacationStatus")
+    VacationResponse toResponse(Vacation vacation);
 }
